fd74c48102fa102b472c2e21726f1db6a9074652
juananPROFE <juanantoniolm@gmail.com>
2021-01-26 12:12:34 +0100

    Renovando versión

--- paqueteProfe/Ejercicio1PROFE.java
+++ paqueteProfe/Ejercicio1PROFE.java
@@ -4,6 +4,7 @@ public class Ejercicio1PROFE {
 	public static void main (String[] args) {
 		System.out.println("Hola caracola");
 		System.out.println("Adios blancaflor");
+		System.out.println("New version ;)");
 		
 	}
 }
