18b44b83c20ae86f2842b00fc788c3a7c6944a0b
juananPROFE <juanantoniolm@gmail.com>
2021-01-26 11:53:25 +0100

    Añadido HolaMundo.java y el paquete del proyecto del PROFE

---
+++ paqueteProfe/Ejercicio1PROFE.java
@@ -0,0 +1,7 @@
+package paqueteProfe;
+
+public class Ejercicio1PROFE {
+	public static void main (String[] args) {
+		System.out.println("Hola caracola");
+	}
+}
---
+++ paqueteProfe/Ejercicio2PROFE.java
@@ -0,0 +1,10 @@
+package paqueteProfe;
+
+public class Ejercicio2PROFE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adiós blancaflor");
+	}
+
+}
