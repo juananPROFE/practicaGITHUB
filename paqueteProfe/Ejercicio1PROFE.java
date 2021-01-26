5c3a415f9531b906c70150629993e221698985e8
juananPROFE <juanantoniolm@gmail.com>
2021-01-26 11:56:12 +0100

    añadido syso al archivo

--- paqueteProfe/Ejercicio1PROFE.java
+++ paqueteProfe/Ejercicio1PROFE.java
@@ -3,5 +3,7 @@ package paqueteProfe;
 public class Ejercicio1PROFE {
 	public static void main (String[] args) {
 		System.out.println("Hola caracola");
+		System.out.println("Adios blancaflor");
+		
 	}
 }
